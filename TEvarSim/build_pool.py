eefa63f5bf84ca48c4c1c84284ab5c4a1cbd1178
Oliver Levine <oliverklevine@uga.edu>
2026-03-18 15:31:28 -0400

    don't apply region restrictions to deletions, only to insertions

--- TEvarSim/build_pool.py
+++ TEvarSim/build_pool.py
@@ -179,12 +179,10 @@ class RandomTE:
                 if end - start < self.DELlen:
                     continue
                 if repClass in self.TEtype:
-                    for region in self.regions:
-                        if region[0] == chrom and region[5] == strand and start >= region[1] and end <= region[2]:
-                            teID = f"DEL-{chrom}-{start}-{end}-{fields[12]}-{fields[10]}"
-                            TEfamily = fields[12]
-                            self.DEL.append((chrom, start, end, teID, TEfamily, "DEL", strand))
-                            continue
+                    teID = f"DEL-{chrom}-{start}-{end}-{fields[12]}-{fields[10]}"
+                    TEfamily = fields[12]
+                    self.DEL.append((chrom, start, end, teID, TEfamily, "DEL", strand))
+                    continue
 
     def parse_DEL_repeatmasker(self):
         """
@@ -210,11 +208,9 @@ class RandomTE:
                 if end - start < self.DELlen:
                     continue
                 if repClass in self.TEtype:
-                    for region in self.regions:
-                        if region[0] == chrom and region[5] == strand and start >= region[1] and end <= region[2]:
-                            teID = f"DEL-{chrom}-{start}-{end}-{fields[10]}-{fields[9]}"
-                            self.DEL.append((chrom, start, end, teID, repClass,"DEL",strand))
-                            continue
+                    teID = f"DEL-{chrom}-{start}-{end}-{fields[10]}-{fields[9]}"
+                    self.DEL.append((chrom, start, end, teID, repClass,"DEL",strand))
+                    continue
     
     def parse_TEpool(self):
         self.INS = []
