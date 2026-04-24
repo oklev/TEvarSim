eb19034040c9a14912ce86044d2381fdc76bdd46
JanMiao <miaojian6363@163.com>
2026-04-24 15:16:49 -0500

    remove TSD in background SV

--- TEvarSim/simulate.py
+++ TEvarSim/simulate.py
@@ -182,6 +182,8 @@ class Simulator:
             chrom, start, end, te_id, strand = event["chrom"], event["start"], event["end"], event["te_id"], event["strand"]
             # tsd length
             tsd_len = np.random.randint(self.tsd_min, self.tsd_max + 1)
+            if te_id.startswith("bg"):
+                tsd_len = 0
             if event["type"] == "INS":
                 # bed file is 0-based
                 ref_allele = self.CHR[chrom]["seq"][start - 1]
